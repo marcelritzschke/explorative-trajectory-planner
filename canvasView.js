a604948d691b724f3b5083a99f201939002af121
Marcel Ritzschke <marcel.ritzschke@mail.de>
2021-05-08 09:38:57 +0800

    # making bower compont # including fabricjs # simplifying mainview

--- canvasView.js
+++ canvasView.js
@@ -1,13 +1,39 @@
-function clearCanvas() {
-    var c = document.getElementById("mainView");
-    var ctx = c.getContext("2d");
-    ctx.clearRect(0, 0, c.width, c.height);
+class View {
+    constructor(canvas) {
+        this._canvas = canvas;
+        this._width = canvas.width;
+        this._height = canvas.height;
+    }
+
+    drawRectangle(top, left, width, height, color) {
+        let canvas = this._canvas;
+        
+        var rect = new fabric.Rect({
+            top : top,
+            left : left,
+            width : width,
+            height : height,
+            fill : color
+        });
+
+        canvas.add(rect);
+    } 
+
+    drawEgoShape() {
+
+    }
 }
 
-function drawDiagonal() {
-    var c = document.getElementById("mainView");
-    var ctx = c.getContext("2d");
-    ctx.moveTo(0, 0);
-    ctx.lineTo(c.width, c.height);
-    ctx.stroke();
+function initializeView() {
+    var elem = document.querySelector('.canvas');
+    var style = getComputedStyle(elem);
+    
+    view = new View(new fabric.Canvas('mainView', {
+        width: parseFloat(style.width),
+        height: parseFloat(style.height)
+    }));
+
+    view.drawRectangle(10, 10, 50, 50, 'red');
 }
+
+let view;
