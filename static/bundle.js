2e9c923179650b81986d1c0bc2a9c98fb7cd302b
Marcel Ritzschke <marcel.ritzschke@mail.de>
2021-11-16 22:22:21 +0800

    introduce dynamic scale for large display resolution

--- static/bundle.js
+++ static/bundle.js
@@ -280,7 +280,7 @@ window.initialize = function() {
   const style = getComputedStyle(elem);
   const width = parseFloat(style.width);
   const height = parseFloat(style.height);
-  const scale = 20; // pixel per meter
+  const scale = getScale(width, height);
 
   canvas = new fabric.Canvas('mainView', {
     width: width,
@@ -294,6 +294,25 @@ window.initialize = function() {
 };
 let canvas;
 let builder;
+
+/**
+ * Calculating the screen size from pixel into metric system.
+ * Up to FHD dimension (1920x1080) the scale will be 20. Exceeding those
+ * dimensions it will be downscaled, that the number of grid cells remains
+ * constant. This is done to reduce load on the processor originated from
+ * the canvas library. It will also keep the space where the planning happens
+ * small.
+ * @param {float} width Screen width in pixel
+ * @param {float} height Screen height in pixel
+ * @return {float} Scale in pixel per meter
+ */
+function getScale(width, height) {
+  let scale = 20;
+  if (width * height > 1920 * 1080) {
+    scale = Math.sqrt(400 * width * height/ (1920 * 1080));
+  }
+  return scale;
+}
 
 // eslint-disable-next-line no-unused-vars
 window.getCanvas = function() {
